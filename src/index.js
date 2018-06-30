5dad203a8c6afe5d8bc858f28e4a9fc99c5831ab
mnjit20 <mnjit.vrma@gmail.com>
2018-07-01 03:07:54 +0530

    props created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const content = 'Hello World';
+const Message = props => <div>{props.children}</div>
+
+const element = (<div className="container">
+  <Message>Hello Worlld</Message>
+  <Message>Worlld again</Message>
+</div>);
+
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
